b45d740e5b7a82fa1e701384c90c276712863e3d
Song Danny <songdanny@Songs-MacBook-Pro.local>
2023-07-30 17:46:49 -0400

    new commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Python-webscraper
+This is my first python project this program will be able to perform a basic web scraping.
---
+++ main.py
@@ -0,0 +1,39 @@
+from requests import get 
+from bs4 import BeautifulSoup
+
+websites = (
+    "https://weworkremotely.com/remote-jobs/search?utf8=%E2%9C%93&term=",
+    "https://www.linkedin.com/jobs/search/?currentJobId=3586717182&keywords=",
+    "https://ca.indeed.com/jobs?q="
+)
+
+website = "https://weworkremotely.com/remote-jobs/search?utf8=%E2%9C%93&term="
+
+key = str(input())
+response = get(f"{website}{key}")
+
+if response.status_code != 200:
+    print("Can't request website")
+else:
+    result = []
+    soup = BeautifulSoup(response.text, "html.parser")
+    jobs =soup.find_all('section',class_="jobs")
+    for job in jobs:
+        job_posts = job.find_all('li')
+        job_posts.pop(-1)
+        for post in job_posts:
+            anchors = post.find_all('a')
+            anchor = anchors[1]
+            link = anchor['href']
+            company, time, region = anchor.find_all('span', class_="company")
+            title = anchor.find('span', class_='title')
+        
+            job_data = {
+                "company": company.string,
+                "time":time.string,
+                "region":region.string,
+                "title":title.string
+            }
+
+            result.append(job_data)
+        print(job_data)
